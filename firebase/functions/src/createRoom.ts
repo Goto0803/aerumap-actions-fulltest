304ac11697ee773ed6e3c30357a00384033909ed
goto <1800syunya@gmail.com>
2025-07-20 11:03:25 +0900

    ggg

--- firebase/functions/src/createRoom.ts
+++ firebase/functions/src/createRoom.ts
@@ -2,7 +2,6 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { Request, Response } from 'express';
 
-
 // Firestore参照
 const db = admin.firestore();
 
